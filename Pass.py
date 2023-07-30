e52e36ea4a0487f10aac0be78fea04ac98f69a94
belfik12 <62045678+belfik12@users.noreply.github.com>
2023-07-30 15:34:09 +0300

    Create Pass.py
    used for your credentials to be stored. for security reason please keep this file in your local device after editing your credentials.

---
+++ Pass.py
@@ -0,0 +1,3 @@
+#python file that is called by the script that needs credential access
+credentials={"ERP_USER":'enter your erp user name',"ERP_USER_PASS":'enter your erp password'
+             ,"away_account":"enter your away user account(i.e email)","away_account_pass":"enter your away account password(i.e email account password"}
